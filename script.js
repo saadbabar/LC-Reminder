881212560b6359d7d847c6fd8eb1da567b8f2a3e
Saad Babar <saadmb@umich.edu>
2024-05-09 17:33:11 -0700

    added api to manifest but errors arose, also added code to fetch api is script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 async function fetchData() {
-    
+    try {
+        // make api request access
+        const response = await fetch('https://alfa-leetcode-api.onrender.com/:username/acSubmission');
+
+
+        // check if response is ok
+
+        if(!response.ok) {
+            throw new Error('Network Error')
+        }
+
+        // Parsing JSON 
+
+        const data = await response.json();
+
+        // Process Data
+        console.log(data);
+    }
+
+    catch (error) {
+        console.error('Error Fetching Data: ', error);
+    }
 }
+
+// Function call
+fetchData();
