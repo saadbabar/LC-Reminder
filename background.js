0ad4ec1f8c2ede1dce0af1fdce8fba5cf32b34d0
omerjunedi <ojunedi@umich.edu>
2024-05-12 01:19:09 -0400

    updated files

--- background.js
+++ background.js
@@ -1,25 +1,53 @@
-// listen to messages from content script
+/* 
+    background.js
+    Created: May 9, 2024
+    Description:
+        Responsible for checking if we're currently on a leetcode problem. Sends a message to the 
+        content script, which manages dom manipulation.
+    Related Files:
+        - content.js: Handles difficulty popup on accepted submissions
+        - index.html: Main extension interface that provides a link to next recommended question
+*/
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if(message.action === 'buttonClicked') {
-        //add logic here
-        console.log('button clicked')
+
+
+
+
+
+
+/*
+    - Fired when URL bards updated
+    - Checks if we're on leetcode.com/problems
+    - Sends message to the content script with the problem name
+
+    URL form: "leetcode.com/problems/<problem name>/"
+
+    references:
+        - tabs.onUpdated: https://developer.chrome.com/docs/extensions/reference/api/tabs#event-onUpdated
+        - tabs.sendMessage: https://developer.chrome.com/docs/extensions/reference/api/tabs#method-sendMessage
+
+    ** potential concerns:
+
+    chrome.onUpdated's listeners triggered whenever a url is updated, so whenever we refresh a page, 
+    click on a link, etc., this listeners fired. so this listeners gonna be fired a lot. idk if this 
+    is a concern tho.
+
+*/
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    
+    if (changeInfo.url && changeInfo.url.includes("leetcode.com/problems/") && changeInfo.status === 'complete') {
+        console.log("Navigated to a LC problem: ", changeInfo.url);
+  
+        // Extract the problem name from the URL (might need to error handle??)
+        const urlParts = changeInfo.url.split('/');
+        const problemIndex = urlParts.indexOf('problems') + 1;
+        let problemName = urlParts[problemIndex];
+        
+        // Send msg to the content script
+        chrome.tabs.sendMessage(tabId, {
+            type: 'ACTIVE PROBLEM',
+            problem: problemName
+        });
     }
-})
-
-chrome.runtime.onMessage.addListener(async function(message, sender, sendResponse) {
-    console.log("this is the onMessage Listener firing")
-    if (message.action === "showPopup") {
-    //   sendResponse({})
-      // add logic to showPopup
-    }
-});
-
-
-//manage popup that appears when clicking submit on leetcode
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === 'showPopup') {
-        chrome.action.openPopup();
-    }
-});
+    // do jack shit when we're not on an active problem
+  });// listen to messages from content script
--- content.js
+++ content.js
@@ -1,66 +1,56 @@
 // // Inject button onto leetcode page
 
-//gets current url: from stackoverflow 
-//https://stackoverflow.com/questions/1979583/how-can-i-get-the-url-of-the-current-tab-from-a-google-chrome-extension
-const getLastFocusedWindowUrl = async() =>  {
-    // see the note below on how to choose currentWindow or lastFocusedWindow
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    console.log(tab.url);
-    return tab.url;
-  }
-
-(async () => {
-    const currentURL = await getLastFocusedWindowUrl()
-    if (currentURL.includes("leetcode.com")) {
-        console.log("we are on leetcode right now")
-        chrome.runtime.sendMessage({ action: "showPopup" }, function(response) {
-            if (chrome.runtime.lastError) {
-              console.error("Error sending message:", chrome.runtime.lastError.message);
-            } else {
-              console.log("Message sent successfully!");
-            }
-          });
-          
-    } else {
-        console.log("we are not on leetcode")
-    }    
-})()
-
-
-
-const button = document.createElement('button');
-button.textContent = 'Click here';
-document.body.appendChild(button);
-
-//send message to background script when button is clicked
-
-button.addEventListener('click', () => {
-    chrome.runtime.sendMessage({action: 'buttonClicked'});
-});
-
-// gets user input from the form
-document.getElementById('myForm').addEventListener('submit', getUsername);
 
 function getUsername() {
-    event.preventDefault();
-    var userName = document.getElementById('usernameinput').value;
-    // Do something with the user input, such as sending it to your API
-    console.log(userName)
-    document.getElementById('output').innerText = 'You entered: ' + userName;
-   
+  event.preventDefault();
+  var userName = document.getElementById('usernameinput').value;
+  // Do something with the user input, such as sending it to your API
+  console.log(userName)
+  document.getElementById('output').innerText = 'You entered: ' + userName;
 }
 
 
-// injecting code into leetcode webpage, for when user clicks submit
-
-document.addEventListener('DOMContentLoaded', function() {
-  //find submit button on leetcode page and attach an event listener
-
-  const lcSubmitButton = document.querySelector('console-submit-button');
-  lcSubmitButton.addEventListener('click', function() {
-    //send message to background script
-
-    chrome.runtime.sendMessage({action: 'showPopup'})
-  })
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+// Check the message action
+if (message.action === "showPopup") {
+  // Your code to show the popup
+  alert("Submit button clicked on LeetCode!");
+}
 })
 
+
+(() => {
+let cur_problem = "";
+
+/*
+  Gets message from background script, alerting that we're on a current problem
+
+  Message types (type):
+    - ACTIVE PROBLEM: attaches a listener for the submit button
+    ...
+*/
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, problem } = obj;
+
+  if (type === "ACTIVE PROBLEM") {
+    cur_problem = problem;
+    attachSubmitListener();
+  }
+});
+
+// Function creates a listener for submit clicks
+function attachSubmitListener() {
+  const submitButton = document.querySelector('button[data-e2e-locator="console-submit-button"]'); 
+
+  submitButton.removeEventListener('click', handleSubmit); // sanity
+  submitButton.addEventListener('click', handleSubmit);
+}
+
+function handleSubmit() {
+    console.log(`${cur_problem} submitted`);
+    // check if submit was accepted, if so, modify dom to add the difficulty popup
+    // am hella lazy so i'll do this tmrw
+}
+
+})()
